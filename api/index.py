9c6edc3989852d16f8a82d03ae914065588eefbe
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-13 19:25:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template_string, render_template, request
 import os
 import requests
 
-os.chdir("/var/task/")
 
-
-app = Flask(__name__, template_folder='api/htmls/')
+app = Flask(__name__, template_folder='/var/task/api/htmls/')
 
 @app.route('/')
 def home():
-    return render_template("/var/task/api/htmls/index.html")
+    return render_template("index.html")
 
 @app.route('/cd')
 def cd():
