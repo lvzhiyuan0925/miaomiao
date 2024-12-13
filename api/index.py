3c986bdc5097ec1e1edacf16a69cc2271484da59
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-13 17:02:40 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='api/htmls/')
 
 @app.route('/')
 def home():
-    return render_template_string(requests.get("https://raw.githubusercontent.com/lvzhiyuan0925/miaomiao/refs/heads/main/api/htmls/index.html"))
+    return render_template_string(requests.get("https://raw.githubusercontent.com/lvzhiyuan0925/miaomiao/refs/heads/main/api/htmls/index.html").text)
 
 @app.route('/about')
 def about():
