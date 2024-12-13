0aea9320820fd2690357baec757966079ccc551f
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-13 16:35:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print(os.listdir("api/htmls/"))
-    return render_template(os.listdir("api/htmls/")[0])
+    return render_template("api/htmls/"+os.listdir("api/htmls/")[0])
 
 @app.route('/about')
 def about():
