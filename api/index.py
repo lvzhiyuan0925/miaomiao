b7bcb91115f48b23cbbb93baefc18b8ad1965ffa
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-13 16:46:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='api/htmls')
+app = Flask(__name__, template_folder='api/htmls/')
 
 @app.route('/')
 def home():
