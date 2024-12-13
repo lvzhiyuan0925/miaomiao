606d228dd36eafaf1b7cfbf272db44e14a9dd383
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-13 18:20:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='api/htmls/')
 @app.route('/')
 def home():
     print(os.getcwd())
-    print("瞄："+os.listdir("/var/task")[0])
+    print("瞄："+os.listdir("/var/task/.git")[0])
     return render_template_string(requests.get("https://raw.githubusercontent.com/lvzhiyuan0925/miaomiao/refs/heads/main/api/htmls/index.html").text)
 
 @app.route('/about')
