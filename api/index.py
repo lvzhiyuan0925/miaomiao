a645b0351d7fab749a2dd14b35124c2d74bdb272
lvzhiyuan <153172321+lvzhiyuan0925@users.noreply.github.com>
2024-12-14 21:23:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template_string, render_template, request
 import os
 import requests
 
+os.chdir('/var/task/api/')
 
 app = Flask(__name__, template_folder='/var/task/api/htmls/')
 
